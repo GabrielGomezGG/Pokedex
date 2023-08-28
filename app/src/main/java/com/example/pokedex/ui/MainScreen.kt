79f28995543a76a9034eabe7408b36e1c8080b07
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-08-28 00:14:15 -0300

    feat: PokemonCard

--- app/src/main/java/com/example/pokedex/ui/MainScreen.kt
+++ app/src/main/java/com/example/pokedex/ui/MainScreen.kt
@@ -1,16 +1,28 @@
 package com.example.pokedex.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pokedex.data.models.Pokemon
 
 @Composable
 fun MainScreen(pokedexViewModel: PokedexViewModel) {
@@ -28,12 +40,34 @@ fun MainScreen(pokedexViewModel: PokedexViewModel) {
             }
         }
         is PokedexUiState.Success -> {
-            LazyColumn(){
+//            LazyColumn(){
+//                items((response as PokedexUiState.Success).pokemonList){
+//                    PokemonCard(pokemon = it)
+//                }
+//            }
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(minSize = 128.dp)
+            ){
                 items((response as PokedexUiState.Success).pokemonList){
-                    Text(text = it.name)
+                    PokemonCard(pokemon = it)
                 }
             }
         }
     }
+}
 
+@Composable
+fun PokemonCard(pokemon : Pokemon) {
+    Card {
+        Column(Modifier.padding(8.dp)) {
+
+            Text(
+                text = pokemon.name,
+                Modifier
+                    .fillMaxWidth(),
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
